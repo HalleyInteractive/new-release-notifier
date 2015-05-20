925e6af3515b610095c5d6358da370a40e443d4d
Niels Overwijn <niels.overwijn@gmail.com>
2015-05-20 23:02:39 +0200

    Use get instead of query (Only getting one user)

--- public/app/components/profile/profile-controller.js
+++ public/app/components/profile/profile-controller.js
@@ -2,6 +2,6 @@
     'use strict';
     angular.module('newReleaseNotifier').controller('ProfileCtrl', function($scope, Profile)
     {
-        $scope.user = Profile.query();
+        $scope.user = Profile.get();
     });
 }());
