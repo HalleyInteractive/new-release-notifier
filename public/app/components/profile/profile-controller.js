3f7502927bfb470e84b4a0e2e802c31a31d911e7
Niels Overwijn <niels.overwijn@gmail.com>
2015-05-22 21:14:13 +0200

    Show notification after saving users profile

--- public/app/components/profile/profile-controller.js
+++ public/app/components/profile/profile-controller.js
@@ -1,7 +1,12 @@
 (function() {
     'use strict';
-    angular.module('newReleaseNotifier').controller('ProfileCtrl', function($scope, Profile)
+    angular.module('newReleaseNotifier').controller('ProfileCtrl', function($scope, Profile, ngNotify)
     {
         $scope.user = Profile.get();
+
+        $scope.profileSaveHandler = function()
+        {
+            ngNotify.set('Your settings have been saved!', 'success');
+        };
     });
 }());
