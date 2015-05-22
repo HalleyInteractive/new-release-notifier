b643bf54aa9931bac57ded871ed0c7280dd30bb8
Niels Overwijn <niels.overwijn@gmail.com>
2015-05-22 20:57:46 +0200

    Added route to save users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,10 +2,27 @@
 
 var express = require('express');
 var router = express.Router();
+var User = require(__dirname + '/../../db/user.js');
 
 router.get('/api/profile', function(req, res, next)
 {
 	res.json(req.user);
+});
+
+router.post('/api/profile', function(req, res, next)
+{
+	User.update({_id:req.user._id}, req.body, function(err)
+	{
+		if(err)
+		{
+			console.log('Error updating'); console.log(err);
+			res.sendStatus(500);
+		} else {
+			console.log('User profile updated');
+			res.status(200);
+			res.json(req.body);
+		}
+	});
 });
 
 module.exports.router = router;
