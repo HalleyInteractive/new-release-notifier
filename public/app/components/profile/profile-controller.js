6e9a8d4859745032dbc3165291990ab1f5de2016
Niels Overwijn <niels.overwijn@gmail.com>
2015-05-22 22:27:54 +0200

    Retrieve pushbullet token on entering accesstoken for pushbullet

--- public/app/components/profile/profile-controller.js
+++ public/app/components/profile/profile-controller.js
@@ -1,14 +1,39 @@
 (function() {
     'use strict';
-    angular.module('newReleaseNotifier').controller('ProfileCtrl', function($scope, Profile, ngNotify)
+    angular.module('newReleaseNotifier').controller('ProfileCtrl', function($scope, $http, Profile, ngNotify)
     {
         $scope.pageTitle = "Profile";
+        $scope.pushbulletDeviceList = [];
         $scope.user = Profile.get();
 
         $scope.profileSaveHandler = function()
         {
             // TODO: Check status code before saying everything have been saved ;)
             ngNotify.set('Your settings have been saved!', 'success');
+        };
+
+        $scope.getPushbulletDeviceList = function()
+        {
+             $http.get('/api/profile/pushbullet/devicelist')
+            .success($scope.getPushbulletDeviceListSuccess)
+            .error($scope.getPushbulletDeviceListError);
+        };
+
+        $scope.getPushbulletDeviceListSuccess = function(response)
+        {
+            if(response !== '')
+            {
+                // TODO: Save complete device to users profile. Including the nickname
+                $scope.pushbulletDeviceList = response.devices;
+            } else {
+                console.log("Accesstoken incorrect");
+                ngNotify.set('Accesstoken incorrect', 'warn');
+            }
+        };
+
+        $scope.getPushbulletDeviceListError = function(error)
+        {
+            ngNotify.set('Error getting your decice list from Pushbullet', 'error');
         };
     });
 }());
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -19,10 +19,37 @@ router.post('/api/profile', function(req, res, next)
 			res.sendStatus(500);
 		} else {
 			console.log('User profile updated');
+
 			res.status(200);
 			res.json(req.body);
 		}
 	});
+});
+
+router.get('/api/profile/pushbullet/devicelist', function(req, res, next)
+{
+	if(req.user.notificationproviders)
+	{
+		if(req.user.notificationproviders.pushbullet)
+		{
+			if(req.user.notificationproviders.pushbullet.accesstoken)
+			{
+				var PushBullet = require('pushbullet');
+				var pusher = new PushBullet(req.user.notificationproviders.pushbullet.accesstoken);
+				pusher.devices(function(error, response) {
+				    console.log("Pushbullet device list response");
+					console.log(response);
+					res.json(response);
+				});
+			} else {
+				res.sendStatus(500);
+			}
+		} else {
+			res.sendStatus(500);
+		}
+	} else {
+		res.sendStatus(500);
+	}
 });
 
 module.exports.router = router;
