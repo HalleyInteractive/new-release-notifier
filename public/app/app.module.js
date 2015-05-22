0c614dc3c1b5a1e6eb00abdf34a69ad19080d855
Niels Overwijn <niels.overwijn@gmail.com>
2015-05-22 21:03:56 +0200

    Included ng-notify files

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,4 +1,4 @@
 (function() {
     'use strict';
-    angular.module('newReleaseNotifier', ['ngRoute', 'ngResource']);
+    angular.module('newReleaseNotifier', ['ngRoute', 'ngResource', 'ngNotify']);
 }());
