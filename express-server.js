62509b4268e6572f5e5a2925ca54110c5cc47dc9
Niels Overwijn <niels.overwijn@gmail.com>
2015-05-24 10:54:43 +0200

    reenable favicon in express

--- express-server.js
+++ express-server.js
@@ -31,7 +31,7 @@ var sessionMiddleware = session(
 var app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 //app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
