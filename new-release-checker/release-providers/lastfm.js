7423a366d56cb2bf0766a8477687f62015774e87
Niels Overwijn <niels.overwijn@gmail.com>
2015-05-24 10:31:13 +0200

    Locally use the secrets file for the LastFM API key

--- new-release-checker/release-providers/lastfm.js
+++ new-release-checker/release-providers/lastfm.js
@@ -10,11 +10,20 @@ module.exports = function()
 
 	this.getNewReleases = function (user, callback)
 	{
+		var lastfmApiKey = '';
+		if(global.nrn.environment == 'local')
+		{
+			var SecretSettingsFile = require(__dirname + '/../../secrets.json');
+			lastfmApiKey = SecretSettingsFile.auth.lastfm.api_key;
+		} else {
+			lastfmApiKey = process.env.LASTFM_API_KEY;
+		}
+		
 		var URLQuery = scope.queryString.stringify(
 		{
 			method: 'user.getnewreleases',
 			user: user.providers.lastfm.username,
-			api_key: process.env.LASTFM_API_KEY,//user.providers.lastfm.apikey,
+			api_key: lastfmApiKey,//user.providers.lastfm.apikey,
 			format: 'json'
 		});
 		scope.request.get({url:scope.endpoint + '?' + URLQuery, json:true}, callback);
